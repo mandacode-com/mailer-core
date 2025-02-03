b154fb31ac39c0106351c2203baba3c3169dd6fe
sauce-Git <dadad1004@naver.com>
2025-02-03 17:21:31 +0900

    feat: add config schema validation from zod

--- src/config/validate.ts
+++ src/config/validate.ts
@@ -1,6 +1,4 @@
-import { Logger } from '@nestjs/common';
-import { IConfig } from 'src/types/config';
-import { validateConfig } from 'src/types/validations/config.validate';
+import { Config, configSchema } from 'src/schemas/config.schema';
 
 const isTrue = (
   value: string | undefined,
@@ -14,25 +12,22 @@ const isTrue = (
   throw new Error('Invalid boolean value');
 };
 
-const logger = new Logger();
-
 export function validate(raw: Record<string, unknown>) {
-  const config: IConfig = {
+  const config: Config = {
+    server: {
+      nodeEnv: raw.NODE_ENV as string,
+      port: parseInt(raw.PORT as string),
+    },
     mailer: {
       host: raw.MAILER_HOST as string,
-      port: parseInt(raw.MAILER_PORT as string, 10),
+      port: parseInt(raw.MAILER_PORT as string),
       secure: isTrue(raw.MAILER_SECURE as string),
-      auth: {
-        user: raw.MAILER_USER as string,
-        pass: raw.MAILER_PASS as string,
-      },
+      user: raw.MAILER_USER as string,
+      pass: raw.MAILER_PASS as string,
     },
   };
-  const result = validateConfig(config);
-  if (result.success) {
-    return config;
-  }
-  const errorPath = result.errors.map((error) => error.path).join(', ');
-  logger.error(`Validation failed for ${errorPath}`);
-  throw new Error('Config validation failed');
+
+  const parsedConfig = configSchema.parse(config);
+
+  return parsedConfig;
 }
---
+++ src/schemas/config.schema.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+export const configSchema = z.object({
+  server: z.object({
+    nodeEnv: z
+      .string()
+      .nonempty()
+      .transform((x) => x.toLowerCase())
+      .refine((x) => ['development', 'production', 'test'].includes(x), {
+        message:
+          'NODE_ENV must be one of "development", "production", or "test"',
+      })
+      .default('development'),
+    port: z.number().int().positive().default(3000),
+  }),
+  mailer: z.object({
+    host: z.string().nonempty().default('smtp.zoho.email'),
+    port: z.number().int().positive().default(587),
+    secure: z.boolean().default(false),
+    user: z.string().nonempty(),
+    pass: z.string().nonempty(),
+  }),
+});
+
+export type Config = z.infer<typeof configSchema>;
